ab67131429dd8a839861004998256507fd5f0542
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2021-09-24 23:15:45 +0700

    Update One.kt

--- One.kt
+++ One.kt
@@ -2,6 +2,5 @@ fun main(args: Array<String>){
     val a = Integer.valueOf(readLine())
     val b = Integer.valueOf(readLine())
     val c = Integer.valueOf(readLine())
-    val result =  (a*a*a)+(b*b)+c
-    print(result)
+    print((a*a*a)+(b*b)+c)
 }
