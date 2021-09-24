8aa60e5aaf791377c506114da4c09bf29ff8a929
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2021-09-24 22:38:28 +0700

    Create One.kt

---
+++ One.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    val a = Integer.valueOf(readLine())
+    val b = Integer.valueOf(readLine())
+    val c = Integer.valueOf(readLine())
+    val result =  (a*a*a)+(b*b)+c
+    print(result)
+}
