9f4e288ddf509a0c10a073936fe275cf2c59c74e
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2021-10-13 00:20:13 +0700

    Delete Four.kt

--- Four.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>){
-    val a:String = readLine()!!
-    val a1f = Integer.parseInt(a[0].toString())
-    val a2f = Integer.parseInt(a[1].toString())
-    println(a1f + a2f)
-}
