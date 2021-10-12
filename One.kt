3a14de28e1a703e12457fb04dfabc187d54990dd
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2021-10-13 00:20:19 +0700

    Delete One.kt

--- One.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>){
-    val a = Integer.valueOf(readLine())
-    val b = Integer.valueOf(readLine())
-    val c = Integer.valueOf(readLine())
-    print((a*a*a)+(b*b)+c)
-}
